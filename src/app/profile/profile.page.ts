136fff39f304be2de8701b287f4ec6fa43dc0519
Inma Caleron <inmacale@gmail.com>
2023-11-06 13:42:40 +0100

    app termonada

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -25,8 +25,10 @@ export class ProfilePage implements OnInit {
       this.url = this.url + id;
     }
 
-    this.htpp.get<any>(this.url).subscribe(res => { console.log(res); this.pokemon = res.results; })
+    this.htpp.get<any>(this.url).subscribe(res => { console.log(res); this.pokemon = res; })
   }
+
+
 }
 
 
